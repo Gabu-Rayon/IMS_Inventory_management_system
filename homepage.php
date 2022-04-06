aa97fa7c050fbb7b35dfe358d5420a519c4e5dae
Gabu-Rayon <73939430+Gabu-Rayon@users.noreply.github.com>
2022-04-06 09:06:26 +0300

    success

---
+++ homepage.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMS Homepage</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://use.fontawesome.com/0c7a3095b5.js"></script>
+</head>
+
+<body>
+    <div class="header">
+        <a href="sign_in.php">Sign In</a>
+    </div>
+    <div class="banner">
+        <div class="homepageContainer">
+            <div class="bannerHeader">
+                <h1>IMS</h1>
+                <p>Inventory Managment System</p>
+            </div>
+            <p class="bannerTagline">
+                Track your goods throughtout your entire supply chain,from purchasing to production to end sales
+            </p>
+            <div class="bannerIcons">
+                <a href=""><i class="fa fa-apple"></i></a>
+                <a href=""><i class="fa fa-android"></i></a>
+                <a href=""><i class="fa fa-windows"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="homepageContainer">
+        <div class="homepageFeatures">
+            <div class="homepageFeature">
+                <span class="featureIcon"><i class="fa fa-gear"></i></span>
+                <h3 class="featureTitle">Editable Theme</h3>
+                <p class="featureDescription">
+                    Lorem ipsum sit amet,concsecturer adispiscing suppps ply it when there enought to use and retrun
+                </p>
+            </div>
+            <div class="homepageFeature">
+                <span class="featureIcon"><i class="fa fa-star"></i></span>
+                <h3 class="featureTitle">Flat Design</h3>
+                <p class="featureDescription">
+                    Lorem ipsum sit amet,concsecturer adispiscing suppps ply it when there enought to use and retrun
+                </p>
+            </div>
+            <div class="homepageFeature">
+                <span class="featureIcon"><i class="fa fa-globe"></i></span>
+                <h3 class="featureTitle">Reach Your Audience</h3>
+                <p class="featureDescription">
+                    Lorem ipsum sit amet,concsecturer adispiscing suppps ply it when there enought to use and retrun
+                </p>
+            </div>
+        </div>
+
+    </div>
+    <div class="homepageNotified">
+        <div class="homepageContainer">
+            <div class="homepageNotifiedContainer">
+                <div class="emailForm">
+                    <h3>Get Notified of Any Updates</h3>
+                    <p>
+                        Lorem ipsum sit amet,concsecturer adispiscing suppps ply it when there enought to use and retrun Lorem ipsum sit amet,concsecturer adispiscing suppps ply it when there enought to use and retrun Lorem ipsum sit amet,concsecturer adispiscing suppps ply
+                        it when there enought to use and retrun Lorem ipsum sit amet,concsecturer adispiscing suppps ply it when there enought to use and retrun
+                    </p>
+                    <form action="">
+                        <div class="formContainer">
+                            <input type="text" placeholder="Email Address">
+                            <button>Notify</button>
+                        </div>
+
+                    </form>
+                </div>
+                <div class=" video ">
+                    <iframe src="http://www.yutubue.com/embed/43243967235" frameborder="0" width="500px" height="300px">
+
+                </iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="socials">
+        <div class="homepageContainer">
+            <h3 class="socialHeader">Say Hi & Get in Touch </h3>
+            <p class="socialText">
+                it when there enought to use and retrun Lorem ipsum sit amet,concsecturer adispiscing suppps ply it when there enought to use and retrun
+            </p>
+            <div class="socialIconsContainer">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-pinterest"></i></a>
+                <a href=""><i class="fa fa-google-plus"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
+                <a href=""><i class="fa fa-youtube"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+
+        <a href="">Contact</a>
+        <a href="">Download</a>
+        <a href="">Press</a>
+        <a href="">Email</a>
+        <a href="">Support</a>
+        <a href="">Privacy</a>
+
+    </div>
+
+
+
+</body>
+
+</html>
