f3a0d18d16f87f98befc90b40bbbe689c222a159
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-04-26 10:49:44 +0300

    new commit

--- inc/container.php
+++ inc/container.php
@@ -4,7 +4,7 @@
     <nav role="navigation" class="navbar navbar-dark navbar-static-top bg-primary bg-gradient">
         <div class="container">
             <div class="navbar-header">
-                <a href="https://www.sourcecodester.com" class="navbar-brand">Inventory Management System - PHP</a>
+                <a href="https://www.sourcecodester.com" class="navbar-brand">Inventory Management System</a>
             </div>
         </div>
     </nav>
